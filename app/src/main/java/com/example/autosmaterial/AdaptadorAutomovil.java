c58dd199ddf770fa36ca820681c612b84450e641
andres pena sierra <apena19@andress-MacBook-Pro.local>
2020-11-10 14:25:34 -0500

    delete Crear.java and .xml (temporal)

--- app/src/main/java/com/example/autosmaterial/AdaptadorAutomovil.java
+++ app/src/main/java/com/example/autosmaterial/AdaptadorAutomovil.java
@@ -1,11 +1,8 @@
 package com.example.autosmaterial;
 
-import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
--- app/src/main/java/com/example/autosmaterial/MainActivity.java
+++ app/src/main/java/com/example/autosmaterial/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.autosmaterial;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -9,21 +8,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements AdaptadorAutomovil.OnAutomovilClickListener{
     private RecyclerView listadoAutomovil;
     private AdaptadorAutomovil adaptador;
     private LinearLayoutManager llm;
     private ArrayList<Automovil> automovil;
-    private Context contexto;
-    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +29,10 @@ public class MainActivity extends AppCompatActivity implements AdaptadorAutomovi
 
         automovil = Datos.obtener();
         automovil = new ArrayList<Automovil>();
-        automovil.add(new Automovil(R.drawable.carro1, "S45123", "FORD", "1998", "rojo", "46993000" ));
-        automovil.add(new Automovil(R.drawable.carro2, "ABC123", "MAZDA", "2998", "blanco", "20000000" ));
-        automovil.add(new Automovil(R.drawable.carro3, "VBC346", "RENAULT", "3998", "negro", "98600000" ));
-        automovil.add(new Automovil(R.drawable.carro4, "TZU022", "RENAULT", "34552", "azul", "120000000" ));
+        automovil.add(new Automovil(R.drawable.carro3, "ABC123", "MAZDA", "1998", "Blanco", "78383000" ));
+        automovil.add(new Automovil(R.drawable.carro1, "SB653S", "KIA", "1972", "Rojo", "63878822" ));
+        automovil.add(new Automovil(R.drawable.carro4, "S45123", "FORD", "2008", "blanco", "25626700" ));
+        automovil.add(new Automovil(R.drawable.carro2, "TZU022", "DODGE", "1998", "rojo", "120000000" ));
 
         adaptador = new AdaptadorAutomovil(automovil, this);
         llm = new LinearLayoutManager(this);
